197a7beda0a937bfb9bca85bae26f9ca0a414fca
Tudose Vlad <vlad.v.tudose@gmail.com>
2013-04-20 23:47:18 +0000

    Cleaned up the little code that we have

--- musiclib/run.sh
+++ musiclib/run.sh
@@ -3,9 +3,22 @@
 PLAYLIST_FLAGS="--random --loop"
 PLAYLIST_FILE=/radio/playlist.xspf
 
-SCRIPT=$(readlink -f $0)
-SCRIPTPATH=`dirname $SCRIPT`
+# Audio parameters
+CODEC=s16l
+CHANNELS=1
+SAMPLERATE=22050
+FREQUENCY=100
+MUX=wav
 
-sudo -u pi cvlc $PLAYLIST_FILE $PLAYLIST_FLAGS --sout "#transcode{acodec=s16l,channels=1,samplerate=22050}:std{access=file,mux=wav,dst=-}" -I http --rc-host 10.10.99.45:8080 | sudo nice -n -20 pifm - 100 22050
+# VLC operations
+TRANSCODE="#transcode{acodec=$CODEC,channels=$CHANNELS,samplerate=$SAMPLERATE}"
+OUTPUT="std{access=file,mux=$MUX,dst=-}"
 
-#vlc --sout "#transcode{acodec=s16l,channels=1,samplerate=22050}:std{access=file,mux=wav,dst=-}" -I http --rc-host 10.10.99.45:8080 | sudo nice -n -20 ./pifm - 100
+# Server settings
+PROTOCOL=http
+BIND="0:8000"
+
+# User
+USER=pi
+
+sudo -u $USER cvlc $PLAYLIST_FILE $PLAYLIST_FLAGS --sout "$TRANSCODE:$OUTPUT" -I $PROTOCOL --rc-host $BIND | sudo nice -n -20 pifm - $FREQUENCY $SAMPLERATE
