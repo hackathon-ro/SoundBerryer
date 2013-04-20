a88620fed5d591f7106f1eef1ec2bd5c6f4b38fd
Tudose Vlad <vlad.v.tudose@gmail.com>
2013-04-20 20:26:24 +0000

    Added music database indexer

---
+++ musiclib/index_musicdb.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#
+# At startup, perform indexing of mp3 files in the music database and use it
+# to build the default playlist
+#
+
+# Note: playlist is in xspf format
+
+LOCATION=/radio/store
+PLAYLIST_FILE=playlist.xspf
+
+cat > $PLAYLIST_FILE << EOL
+<?xml version="1.0" encoding="UTF-8"?>
+<playlist xmlns="http://xspf.org/ns/0/" xmlns:vlc="http://www.videolan.org/vlc/playlist/ns/0/" version="1">
+	<title>Default playlist</title>
+        <trackList>
+EOL
+
+find $LOCATION -name \*.mp3 -exec echo "<track> <location>file://{}</location> </track>" >> $PLAYLIST_FILE \;
+
+cat >> $PLAYLIST_FILE << EOL
+	</trackList>
+</playlist>
+EOL
